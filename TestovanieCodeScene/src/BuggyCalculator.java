228b84a8a131a4f288e297309564ae1a6f2c3b0c
Patrik Gereg <p_gereg@utb.cz>
2025-05-14 14:47:47 +0200

    Add a Subtract

--- TestovanieCodeScene/src/BuggyCalculator.java
+++ TestovanieCodeScene/src/BuggyCalculator.java
@@ -1,9 +1,15 @@
 ﻿public class BuggyCalculator {
+    private int lastResult = 0; // Nepoužívaná premenná
+
     public int add(int a, int b) {
         return a + b;
     }
 
     public int subtract(int a, int b) {
         return a - b;
+    }
+
+    public int MULTIPLY(int a, int b) { // Porušenie camelCase
+        return a * b;
     }
 }
